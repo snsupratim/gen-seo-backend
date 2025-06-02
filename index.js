8cb39b9a5f9e0e6e3d3595baf3175a7df42abdd5
Supratim Nag <snsupratim@gmail.com>
2025-06-02 22:01:56 +0530

    added python sh file

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const bodyParser = require("body-parser");
 const { spawn } = require("child_process");
 const cors = require("cors");
 
+app.use(
+  cors({
+    origin: "https://your-frontend.vercel.app", // replace with actual domain
+  })
+);
+
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
